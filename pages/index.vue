87ff57a3aaa230e316be39f9d0bd4567f6b20de7
jordan <jbs.io@protonmail.com>
2023-03-13 11:43:36 +0100

    Prisma setup

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,30 @@
 <template>
-  <div>
-    <h1>Home</h1>
-    <p>
-      My home page
-    </p>
-  </div>
+  <v-container class="container">
+    <!-- Hero -->
+    <v-row
+      class="mx-auto"
+    >
+      <!-- Text -->
+      <v-col cols="12">
+        <h1 class="text-h2 text-md-h1 mb-8">
+          ioTactile Games
+        </h1>
+        <h2 class="text-h5 text-md-h4 mb-4">
+          Bienvenue à vous
+        </h2>
+        <p class="text-h6 text-md-h5">
+          De nombreux jeux vous attendent pour vous amuser en famille ou entre amis.<br>
+          Vous trouverez les jeux dans la barre de navigation à gauche de votre écran.
+        </p>
+      </v-col>
+      <!-- Logo -->
+      <!-- <img/> -->
+    </v-row>
+  </v-container>
 </template>
+
+<style scoped>
+.container {
+  max-width: 1200px !important;
+}
+</style>
