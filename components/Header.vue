c0b0a5310d1bce63101345112a7b2b6863c55320
jordan <jbs.io@protonmail.com>
2023-05-16 10:33:55 +0200

    fix for laurent

--- components/Header.vue
+++ components/Header.vue
@@ -18,7 +18,7 @@
     </v-app-bar>
     <v-navigation-drawer v-model="drawer" width="200" color="secondary">
       <v-sheet class="d-flex flex-column" height="100%">
-        <v-list nav class="pa-0">
+        <v-list nav class="ml-3">
           <v-list-item
             v-for="(item, i) in items"
             :key="i"
