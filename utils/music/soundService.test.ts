d5c81b80f761fd78a40143d26cd2f05b8f5b793e
ioTactile <jbs.io@protonmail.com>
2023-11-22 14:10:38 +0100

    Add unit tests for classes

---
+++ utils/music/soundService.test.ts
@@ -0,0 +1,72 @@
+import { describe, beforeEach, test, expect, afterEach } from 'vitest'
+import { SoundService } from './soundService'
+
+describe('SoundService', () => {
+  let soundService: SoundService
+  const sounds = [
+    {
+      name: 'Benzaiten Asian Lofi',
+      src: '/music/asian-lofi/Benzaiten Asian Lofi.m4a'
+    },
+    {
+      name: 'Blossom Tree Asian Lofi',
+      src: '/music/asian-lofi/Blossom Tree Asian Lofi.m4a'
+    }
+  ]
+
+  beforeEach(() => {
+    soundService = new SoundService()
+  })
+
+  afterEach(() => {
+    soundService.unloadAllSounds()
+  })
+
+  test('should load sound', () => {
+    soundService.loadSound(sounds[0].name, sounds[0].src, 0.5)
+    expect(soundService.isSoundLoaded(sounds[0].name)).toBe(true)
+  })
+
+  test('should unload sound', () => {
+    soundService.loadSound(sounds[0].name, sounds[0].src, 0.5)
+    soundService.unloadSound(sounds[0].name)
+    expect(soundService.isSoundLoaded(sounds[0].name)).toBe(false)
+  })
+
+  test('should play sound', () => {
+    soundService.loadSound(sounds[0].name, sounds[0].src, 0.5)
+    soundService.playSound(sounds[0].name)
+    expect(soundService.isSoundPlaying(sounds[0].name)).toBe(true)
+  })
+
+  test('should mute sound', () => {
+    soundService.loadSound(sounds[0].name, sounds[0].src, 0.5)
+    soundService.muteSound(sounds[0].name)
+    expect(soundService.isSoundMuted(sounds[0].name)).toBe(true)
+  })
+
+  test('should unmute sound', () => {
+    soundService.loadSound(sounds[0].name, sounds[0].src, 0.5)
+    soundService.muteSound(sounds[0].name)
+    soundService.unmuteSound(sounds[0].name)
+    expect(soundService.isSoundMuted(sounds[0].name)).toBe(false)
+  })
+
+  test('should stop all sounds', () => {
+    soundService.loadSound(sounds[0].name, sounds[0].src, 0.5)
+    soundService.loadSound(sounds[1].name, sounds[1].src, 0.5)
+    soundService.playSound(sounds[0].name)
+    soundService.playSound(sounds[1].name)
+    soundService.stopAllSounds()
+    expect(soundService.isSoundPlaying(sounds[0].name)).toBe(false)
+    expect(soundService.isSoundPlaying(sounds[1].name)).toBe(false)
+  })
+
+  test('should unload all sounds', () => {
+    soundService.loadSound(sounds[0].name, sounds[0].src, 0.5)
+    soundService.loadSound(sounds[1].name, sounds[1].src, 0.5)
+    soundService.unloadAllSounds()
+    expect(soundService.isSoundLoaded(sounds[0].name)).toBe(false)
+    expect(soundService.isSoundLoaded(sounds[1].name)).toBe(false)
+  })
+})
